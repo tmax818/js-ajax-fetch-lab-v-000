505205d311fe7672524185bf46f6026abc3572eb
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-08-29 19:35:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,22 +1,42 @@
 function getIssues() {
+    const repo = 'javascript-fetch-lab/issues/';
+  //use fetch to fork it!
+    fetch(repo, { 
+    method: 'post',
+    headers: { 
+      Authorization: `token ${getToken}` }})
 }
 
 function showIssues(json) {
+  document.getElementById('body').innerHTML = json;
 }
 
 function createIssue() {
+    const repo = '/javascript-fetch-lab/issues/';
+  //use fetch to fork it!
+    fetch(repo, { 
+    method: 'post',
+    body: 'test body',
+    headers: { 
+      Authorization: `token ${getToken}` }}).then(res => res.json()).then(json => showIssues(json))
 }
 
 function showResults(json) {
+  
 }
 
 function forkRepo() {
-  const repo = 'learn-co-curriculum/javascript-fetch-lab'
+  const repo = 'api.github.com/repos/learn-co-curriculum/javascript-fetch-lab/';
   //use fetch to fork it!
+    fetch(repo, { 
+    method: 'post',
+    headers: { 
+      Authorization: `token ${getToken}` }})
 }
 
 function getToken() {
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
-  return ''
+  //return "fd9cd420b859827bc273fb5e31e240eb8566e57c";
+   return "";
 }
